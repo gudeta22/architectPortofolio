da64dfd27257365465d2a5af7814dbc34690db5a
Gudeta Buli <gbteshite6195@gmail.com>
2023-06-29 19:51:59 +0300

    some modifcation on the contact page

--- src/components/ContactNew.js
+++ src/components/ContactNew.js
@@ -37,7 +37,7 @@ function ContactComponent() {
           
           <div className="flex space-x-96 my-16 p-5 justify-center">
             <div className="flex-col space-y-12 my-2 ">
-              <div className="w-[22rem] lg:w-[38rem] md:w-[38rem] m-5">
+              <div className="w-[20rem] lg:w-[38rem] md:w-[38rem] m-8">
                 <div className="flex space-x-5 border p-5 lg:p-8 md:p-8 bg-gray-200 w-auto">
                   <img src={homeicon} alt="" className="h-10 w-10 md:h-8 md:w-8 sm:w-5 sm:h-5 " />
                   <div className="my-2 fontstyle font-semibold">
@@ -45,13 +45,13 @@ function ContactComponent() {
                   </div>
                 </div>
               </div>
-              <div className="flex w-[22rem] lg:w-[38rem] md:w-[38rem] m-5 space-x-3 border p-5 lg:p-8 md:p-8 bg-gray-200  ">
+              <div className="flex w-[20rem] lg:w-[38rem] md:w-[38rem] m-8 space-x-3 border p-5 lg:p-8 md:p-8 bg-gray-200  ">
                 <img src={mail} alt="" className="h-10 w-10 md:h-8 md:w-8 sm:w-5 sm:h-5 " />
                 <div className="my-1 font-semibold fontstyle sm:text-sm lg:text-lg">
                  {Person.email}
                 </div>
               </div>
-              <div className="flex align-middle w-[22rem] lg:w-[38rem] md:w-[38rem] m-5 space-x-5 border p-5 lg:p-8 md:p-8 bg-gray-200  ">
+              <div className="flex align-middle w-[20rem] lg:w-[38rem] md:w-[38rem] m-8 space-x-5 border p-5 lg:p-8 md:p-8 bg-gray-200  ">
                 <img src={phone} alt="" className="h-10 w-10 md:h-8 md:w-8 sm:w-5 sm:h-5 " />
                 <div className="my-2  fontstyle font-semibold">
                   +251-911393207
