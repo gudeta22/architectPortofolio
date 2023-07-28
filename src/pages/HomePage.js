2536c8be4d58db033dd12da58e13ccc7cebdf531
Gudeta Buli <gbteshite6195@gmail.com>
2023-07-28 13:13:23 +0300

    some modification on contact

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,25 +1,18 @@
-import React from 'react';
-import AboutComponent from '../components/AboutComponent';
-import ServiceComponent from '../components/ServiceComponent';
-import OurWork from '../components/ourWork';
-import Aboutwork from '../components/Aboutwork-2';
-import HomePage from '../components/homePageComponnent';
-
+import React from "react";
+import AboutComponent from "../components/AboutComponent";
+import ServiceComponent from "../components/ServiceComponent";
+import OurWork from "../components/ourWork";
+import Aboutwork from "../components/Aboutwork-2";
+import HomePage from "../components/homePageComponnent";
 
 function Home() {
- 
   return (
-    
     <div>
-      <HomePage/>
+      <HomePage />
       <AboutComponent />
       <ServiceComponent />
       <OurWork />
       <Aboutwork />
-      
-      
-  
-      
     </div>
   );
 }
