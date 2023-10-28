42ff4b4e4de36924d270745b89cc0f1d1e8dc65b
Gudeta Buli <gbteshite6195@gmail.com>
2023-10-28 16:26:32 +0300

    some modification

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,12 +1,12 @@
-import React , {useEffect} from "react";
+import React, { useEffect } from "react";
 import About1 from "../assets/images/about1.png";
 import About2 from "../assets/images/about2.png";
 import ButtonComponent from "./ButtonComponent";
 import { Link } from "react-router-dom";
 function AboutPage() {
-    useEffect(() => {
-        window.scrollTo(0, 0);
-    }, []);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
   return (
     <div className="container m-auto">
       <div className=" px-4 mi:flex gap-6 py-36">
@@ -15,20 +15,19 @@ function AboutPage() {
             <div className="w-7 rounded-md mx-2 h-[2px] bg-red-600"></div>
             <p>About Us</p>
           </span>
-          <h2 className=" text-[#1d2434] text-[24px] fontstyle font-extrabold text-5xl lg:text-5xl ">
-          We Create Architectural Masterpieces
+          <h2 className=" text-[#1d2434] text-[24px] fontstyle font-extrabold text-5xl lg:text-2xl ">
+            We Create Architectural Masterpieces
           </h2>
           <p className="poppins font-normal text-[#686868]">
-            In the year 2021 G.C, a group of passionate Architects and Engineers
-            joined forces to establish Misikir Zewdu Architects. Since our
-            inception, we have strived to offer a comprehensive range of design
-            and supervision services catering to projects of diverse magnitudes.
-            Our firm takes pride in being registered, qualified, and composed of
-            a team of dedicated professionals who excel in providing exceptional
-            Architectural, Interior, and Landscape design solutions.
+            Peniel International Church's journey began in 2012 GC when a group
+            of visionary individuals recognized the need for a place where
+            people from various nations could come together in worship and
+            fellowship. From that moment, our church has continued to evolve,
+            embracing the rich tapestry of cultures and traditions that make up
+            our global congregation.
           </p>
-          <Link to="/about" >
-          <ButtonComponent title={"About Us"} />
+          <Link to="/about">
+            <ButtonComponent title={"About Us"} />
           </Link>
         </div>
 
